f3360c506f061f9e1dc2022c055b4561859c824f
KVITFEL <71916434+KVITFEL@users.noreply.github.com>
2022-09-29 17:31:50 +0300

    Update laba6.c

--- laba6.c
+++ laba6.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <locale.h>
 
+void zadanie1()
+{
+	int yeas;
+	puts("Введите год:");
+	scanf("%d", &yeas);
+
+	if (yeas % 4 == 0 && yeas % 100 != 0 || yeas % 400 == 0)
+		printf("Считается високосным годом - %d", yeas);
+	else
+		printf("Считает не високосным - %d ", yeas);
+
+
+
+
+}
+
+
+void main()
+{
+	setlocale(LC_ALL, "ru-RU");
+	zadanie1();
+}
