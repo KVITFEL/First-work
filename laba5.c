12276b55133533ac0470ab07e9ceedcbef9e24fe
KVITFEL <71916434+KVITFEL@users.noreply.github.com>
2022-09-29 17:16:38 +0300

    Update laba5.c

--- laba5.c
+++ laba5.c
@@ -12,8 +12,21 @@ void zadanie1()
 	scanf("%d", &gr);
 	double result = (gr * M_PI / 180);
 	printf("sin : %.6f \n", sin(result));
+}
 
 
+#define c 0.4
+void zadanie2()
+{
+	float x, a, b, y;
+	puts("Введите число");
+	scanf("%f", &x);
+	a = log10(x);
+	//printf("%f", a);
+	b = pow(a,2) + sqrt(c * x);
+	//printf("%f", b);
+	y = (exp(2 * x) + pow(9.7, b));
+	printf("%.2f", y);
 }
 
 
@@ -22,6 +35,6 @@ void zadanie1()
 void main()
 {
 	setlocale(LC_ALL, "ru-RU");
-	zadanie1();
+	zadanie2();
 
 }
