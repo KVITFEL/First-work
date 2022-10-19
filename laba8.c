8e210f9a26a35e688199b38f04067b4a93f9b4f6
KVITFEL <71916434+KVITFEL@users.noreply.github.com>
2022-10-19 20:24:46 +0300

    Update laba8.c

--- laba8.c
+++ laba8.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
 
+void zadanie1()
+{
+    int m,n,i,s;
+    scanf("%d", &n);
+    scanf("%d", &m);
+    for (int i=m, s=0;i>=n;i--){
+        s+=i;
+        printf("выполнено %d раз", i+1);
+    }
+    printf("результат %d", s);
+    
+
+}
+
+void main()
+{
+	setlocale(LC_ALL, "ru-RU");
+	zadanie1();
+}
