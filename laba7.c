dacf684cbfa371d0af1eed4269f9a620c1087fed
KVITFEL <71916434+KVITFEL@users.noreply.github.com>
2022-10-19 17:35:58 +0300

    Update laba7.c

--- laba7.c
+++ laba7.c
@@ -4,20 +4,52 @@
 void zadanie1()
 {
 char c;
-printf("Введите символы 'a' or 'b': ");
+printf("Введите цифру или букву: ");
 scanf("%c",&c);
 switch (c)
 {
-    case 'a':
-    printf("Введено 'a'.\n");
+    case '0':
+    printf("Это цифра '0'.\n");
     break;
     
-    case 'b':
-    printf("Введено 'b'.\n");
+    case '1':
+    printf("Это цифра '1'.\n");
+    break;
+    
+    case '2':
+    printf("Это цифра '2'.\n");
+    break;
+    
+    case '3':
+    printf("Это цифра '3'.\n");
+    break;
+    
+    case '4':
+    printf("Это цифра '4'.\n");
+    break;
+    
+    case '5':
+    printf("Это цифра '5'.\n");
+    break;
+    
+    case '6':
+    printf("Это цифра '6'.\n");
+    break;
+    
+    case '7':
+    printf("Это цифра '7'.\n");
+    break;
+    
+    case '8':
+    printf("Это цифра '8'.\n");
+    break;
+    
+    case '9':
+    printf("Это цифра '9'.\n");
     break;
     
     default:
-    printf("Неизвестный символ\n");
+    printf("Это буква %c \n",c);
     break;
 }
 
