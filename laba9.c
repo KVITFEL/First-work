c90fa6c5304678a2f91fe8591968e31f2f02a1af
KVITFEL <71916434+KVITFEL@users.noreply.github.com>
2022-10-31 20:01:20 +0300

    Update laba9.c

--- laba9.c
+++ laba9.c
@@ -19,75 +19,25 @@ void zadanie1() {
 }
 
 
-void zadanie3() {
-	while (1)
-
-	{
-
-		char a;
-
-		// Решение домашней задачи
-
-		printf("Продолжить ? (Да - y, нет - n)");
-
-		if ((a = getchar()) == 'n') break;
-		else{	float x = 2.06, y = 0;
-
-
-		// esli x == 63 ili 64 to == 0
-		long double sum1 = 1, sum2 = 1;
-
-		for (int i = 1; i < 64; i += 2) {
-			y = x - i;
-			sum1 *= y;
-			printf("x - %i | %.2f - %i = %f | %.5f\n", i, x, i, y, sum1);
-			y = 0;
-		}
-		for (int i = 2; i < 65; i += 2) {
-			y = x - i;
-			sum2 *= y;
-			printf("x - %i | %.2f - %i = %f | %.5f\n", i, x, i, y, sum2);
-			y = 0;
-		}
-		printf("%.5f\n", sum1 / sum2);
-		}
-	}
-}
-
-void zadanie4() {
-	int r;
-  scanf("%i", &r);
-	printf("\n");
-	for (int i = 1; i <= r; ++i) {
-		for (int i = 1; i <= r; ++i) {
-			printf("* ");
-		}
-		printf("\n");
-	}
-
-}
-
-
-void zadaniehz()
+void zadanie3()
 {
-    int a = 9;
-    int ch = '*';
-    
-    for(int i = 1; i <= a; i++)
-    {
-        for(int j = 1; j <= a; j++)
-            if(j == a || i == a || i == a - j + 1)
-                putchar(ch);
-            else
-                putchar(' ');
-        putchar('\n');        
+    int i;
+    int j = 0;
+    char a = getchar();
+    for(i = 0; i < 10; i++) {
+        for(j = 0; j < 30-i*3; j++)putchar(' ');
+        putchar(a);
+        for(j = 0; j < i*3-j;j++)putchar(' ');
+        putchar(a);
+        putchar('\n');
     }
-    
-    return 0;
+    putchar(' ');
+    putchar(' ');
+    for(i = 0; i < 16; i++) putchar(a);
 }
 
 void main()
 {
 	setlocale(LC_ALL, "ru-RU");
-	zadanie4();
+	zadanie3();
 }
