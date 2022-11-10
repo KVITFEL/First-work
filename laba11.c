c4c60e94407f093d1e40769b12c7ebea370fc678
KVITFEL <71916434+KVITFEL@users.noreply.github.com>
2022-11-10 17:57:32 +0300

    Update laba11.c

--- laba11.c
+++ laba11.c
@@ -2,15 +2,15 @@
 #include <locale.h>
 #include <math.h>
 
-
-
-    //zadanie 1 
-/*void main()
+float func1(float x);
+#define N 100
+//zadanie 1 
+void main()
 {
-    #define N 10
-	setlocale(LC_ALL, "ru-RU");
-	float A[N];
-	float temp, sum = 0;
+
+    setlocale(LC_ALL, "ru-RU");
+    float A[N];
+    float temp, sum = 0;
     for (int i = 0; i < N; i++)
     {
         printf("a[%d] = ", i + 1);
@@ -22,22 +22,32 @@
     for (int i = 0; i < N; i++) {
         printf("|    %d   |       %.3f       |      %.3f    |\n", i + 1, A[i], A[i] / 10 );// novoe znachenie
     }
-
     printf("Среднее арифметическое = %.5f", sum / N);
+}
 
-}*/
-
-    //zadanie2
+//zadanie2
 
 void main()
 {
-#define N 100
 
+    setlocale(LC_ALL, "ru-RU");
+    float A[N];
+    float temp = 0.1;
+    float tab, sum = 0;
+    
 
+    for (int i = 0; i < N; temp = temp + tab) {
+        A[i] = func1(temp);
+        sum = sum + A[i];
+        printf("\n");
+        printf("| индекс | исходное значение | \n");
+        printf("|    %d   |       %.3f       |\n", i + 1, A[i]);
+        printf("Среднее арифметическое = % .5f", sum / N);
 
-
+    }
 }
 
 float func1(float x) {
-
+    float pi = 3.14;
+    return x * x - cos(pi * x) * cos(pi * x);
 }
